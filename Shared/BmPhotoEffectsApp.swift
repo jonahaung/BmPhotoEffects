c0b52afb0ac212981255765d402c8f57e54e42d9
Aung Ko Min <jonahaung@gmail.com>
2022-03-14 13:49:48 +0800

    Initial Commit

---
+++ Shared/BmPhotoEffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BmPhotoEffectsApp.swift
+//  Shared
+//
+//  Created by Aung Ko Min on 14/3/22.
+//
+
+import SwiftUI
+
+@main
+struct BmPhotoEffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
